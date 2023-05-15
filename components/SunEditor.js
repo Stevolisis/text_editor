b508be3f54f5d043de7b4010f3a3061f057683b7
Steven Joseph <stevolisisjoseph@gmail.com>
2023-05-16 00:22:59 +0100

    Sun editor Set

---
+++ components/SunEditor.js
@@ -0,0 +1,8 @@
+import SunEditor from 'suneditor-react';
+import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
+
+export default function SunEditors(){
+    return (
+        <SunEditor />
+    );
+};
